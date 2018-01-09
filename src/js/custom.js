b729f0c789d63781c439ddb21d26d4a172d0036c
Joseph Richey <josephrichey@me.com>
2018-01-09 14:54:54 -0500

    Slick slider bugfix

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,10 @@
 //Example Slick Slider config. Works for most use cases.
-/* $('.slick-slider').slick({
-    autoplay: true,
-    autoplaySpeed: 5000,
-    adaptiveHeight: true 
+/* $(document).ready(function(){
+    $('.slick-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        adaptiveHeight: true 
+    }); 
 }); */
 
 $('.mobile-nav-button').on('click', function(){
